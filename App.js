6ceb965e4b625f4d736fcb8f7b8002a59687b22b
Sid Bentifraouine <sidbentifraouine@gmail.com>
2022-01-31 15:20:14 +0100

    Feature(LoL vs): status bar theme

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
-import { Switch, Button, View, Text, Image } from 'react-native'
+import {
+  StatusBar,
+  Switch,
+  Button,
+  View,
+  Text,
+  Image,
+  ScrollView
+} from 'react-native'
 import Greeting from './components/Greeting'
 import PlayerCard from './components/PlayerCard'
 
@@ -28,47 +36,57 @@ export default function App() {
   }
 
   return (
-    <View
+    <ScrollView
       style={{
-        flex: 1,
-        backgroundColor: theme === 'light' ? 'white' : 'black',
-        alignItems: 'center',
-        justifyContent: 'center'
+        backgroundColor: theme === 'light' ? 'white' : '#0b0b23',
+        paddingTop: 44
       }}
     >
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ fontSize: 24 }}>🌞</Text>
-        <Switch onChange={switchTheme} value={theme == 'dark'}></Switch>
-        <Text style={{ fontSize: 24 }}>🌚</Text>
-      </View>
       <View
         style={{
-          marginTop: 8,
-          alignItems: 'center'
+          flex: 1,
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center'
         }}
       >
-        <PlayerCard
-          username={player1.username}
-          champion={player1.champion}
-          theme={theme}
-        ></PlayerCard>
-        <Image
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={{ fontSize: 24 }}>🌞</Text>
+          <Switch onChange={switchTheme} value={theme == 'dark'}></Switch>
+          <Text style={{ fontSize: 24 }}>🌚</Text>
+        </View>
+        <View
           style={{
-            height: 50,
-            width: 50,
-            marginVertical: 16
+            marginTop: 8,
+            alignItems: 'center'
           }}
-          source={{
-            uri: 'https://www.nicepng.com/png/full/271-2712237_vs-rooster-teeth.png'
-          }}
-        ></Image>
-        <PlayerCard
-          username={player2.username}
-          champion={player2.champion}
-          theme={theme}
-        ></PlayerCard>
+        >
+          <PlayerCard
+            username={player1.username}
+            champion={player1.champion}
+            theme={theme}
+          ></PlayerCard>
+          <Image
+            style={{
+              height: 50,
+              width: 50,
+              marginVertical: 16
+            }}
+            source={{
+              uri: 'https://www.nicepng.com/png/full/271-2712237_vs-rooster-teeth.png'
+            }}
+          ></Image>
+          <PlayerCard
+            username={player2.username}
+            champion={player2.champion}
+            theme={theme}
+          ></PlayerCard>
+        </View>
+        {/* <Button title="🎛 Switch theme" onPress={switchTheme}></Button> */}
+        <StatusBar
+          barStyle={theme == 'light' ? 'dark-content' : 'light-content'}
+        />
       </View>
-      {/* <Button title="🎛 Switch theme" onPress={switchTheme}></Button> */}
-    </View>
+    </ScrollView>
   )
 }
